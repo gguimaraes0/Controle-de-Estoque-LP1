d9085e700e55dac5c40cd4ba89119b790bcd5bd9
Matheus Sarquis <matheussarquis2@gmail.com>
2021-05-26 10:32:29 -0300

    login ta abrindo

--- ControleDeEstoque/wwwroot/js/site.js
+++ ControleDeEstoque/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#loginModal').modal('show');
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+});

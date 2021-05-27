77cbdeadca02464079cf4fa72b30cd5a9bb7f718
Matheus Sarquis <matheussarquis2@gmail.com>
2021-05-26 21:59:43 -0300

    Criação das controllers e abri as views

---
+++ ControleDeEstoque/Controllers/FornecedorController.cs
@@ -0,0 +1,27 @@
+﻿using ControleDeEstoque.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly ILogger<FornecedorController> _logger;
+
+        public FornecedorController(ILogger<FornecedorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CadastroFornecedor()
+        {
+            FornecedoresViewModel fornecedor = new FornecedoresViewModel();
+            return View("CadastroFornecedor", fornecedor);
+        }
+    }
+}
---
+++ ControleDeEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using ControleDeEstoque.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CadastroProduto()
+        {
+            ProdutoViewModel produto = new ProdutoViewModel();
+            return View("CadastroProduto", produto);
+        }
+    }
+}
